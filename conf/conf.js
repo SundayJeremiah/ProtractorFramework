8dd60c2d7200b39fc93c745926dca50595466b1a
Sunday Jeremiah <jsunday1000@yahoo.co.uk>
2020-02-05 23:56:24 +0000

    My first git commit

---
+++ conf/conf.js
@@ -0,0 +1,36 @@
+// An example configuration file.
+exports.config = {
+  directConnect: true,
+
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  // Framework to use. Jasmine is recommended.
+  framework: 'jasmine2',
+
+  // Spec patterns are relative to the current working directory when
+  // protractor is called.
+  specs: ['../tests/calculator.js'],
+
+  // Options to be passed to Jasmine.
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  },
+
+  // Assign the test reporter to each running instance
+  onPrepare: function () {
+    var AllureReporter = require('jasmine-allure-reporter');
+    jasmine.getEnv().addReporter(new AllureReporter());
+    jasmine.getEnv().afterEach(function (done) {
+      browser.takeScreenshot().then(function (png) {
+        allure.createAttachment('Screenshot', function () {
+          return new Buffer(png, 'base64')
+        }, 'image/png')();
+        done();
+      })
+    });
+  },
+
+};
---
+++ tests/calculator.js
@@ -0,0 +1,28 @@
+let homepage = require('../pages/homepage')
+
+describe('demo calculator test', function () {
+
+    it('Addition test', function () {
+        // browser.driver.manage().window().maximize();
+
+        // browser.get('https://juliemr.github.io/protractor-demo/');
+        homepage.get('https://juliemr.github.io/protractor-demo/');
+
+        //element(by.model('first')).sendKeys(2);
+        homepage.enterFirstNumber('3');
+
+        //element(by.model('second')).sendKeys(4);
+        homepage.enterSecondNumber('5');
+
+        //element(by.css('[ng-click="doAddition()"]')).click();
+        homepage.clickGo();
+
+        // let result = element(by.cssContainingText('.ng-binding', '6'));
+        //expect(result.getText()).toEqual('6');
+        homepage.verifyResult('8');
+
+        browser.sleep(2000);
+
+    });
+    
+});
